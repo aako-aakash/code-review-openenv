bf71e3731165efb37d0a0da1cded6d942d40691b
aako-aakash <aako27082004@gmail.com>
2026-04-06 13:26:49 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -25,11 +25,11 @@ def state():
     return env.state()
 
 
-# ✅ REQUIRED MAIN FUNCTION
+#  REQUIRED MAIN FUNCTION
 def main():
     uvicorn.run("server.app:app", host="0.0.0.0", port=8000, reload=False)
 
 
-# ✅ REQUIRED ENTRY POINT
+#  REQUIRED ENTRY POINT
 if __name__ == "__main__":
     main()
